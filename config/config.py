50f92f8f7e9be34fbbfc76f68e42565aad75b307
it21288630 <prabashwara.in@gmail.com>
2024-09-08 07:02:51 +0530

    initial

---
+++ config/config.py
@@ -0,0 +1,14 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://prabashwarakulathunga:JwF4QtpLSHh6919S@autograder.uum3b.mongodb.net/?retryWrites=true&w=majority&appName=autograder"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
