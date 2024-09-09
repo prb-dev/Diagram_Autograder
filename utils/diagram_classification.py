a3cc4c542c2bfc883cd6f038ab65790810896200
it21288630 <prabashwara.in@gmail.com>
2024-09-09 09:34:48 +0530

    Create utils functions

---
+++ utils/diagram_classification.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+import numpy as np
+
+def classify_diagram(image_path):
+    model = YOLO("./ai_models/last.pt")  # load a custom model
+
+    # Predict with the model
+    results = model(image_path)  # predict on an image
+
+    class_names = results[0].names
+    probs = results[0].probs.tolist()
+    predicted_classname = class_names[np.argmax(probs)]
+    
+    return predicted_classname
+
