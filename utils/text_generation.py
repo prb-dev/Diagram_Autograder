8affa93fea0e7d946a632e6e9b23102df0bf52ad
it21288630 <prabashwara.in@gmail.com>
2024-09-17 06:15:18 +0530

    Integrate the VLM

---
+++ utils/text_generation.py
@@ -0,0 +1,18 @@
+from gradio_client import Client, handle_file
+from utils.prompts import get_prompts
+from utils.convert_to_json import get_json
+
+def generate_text(image_url, diagram_type):
+    prompt = get_prompts(diagram_type)
+    
+    client = Client("GanymedeNil/Qwen2-VL-7B")
+    result = client.predict(
+            image=handle_file(image_url),
+            text_input=prompt,
+            model_id="Qwen/Qwen2-VL-7B-Instruct",
+            api_name="/run_example"
+    )
+    
+    result = get_json(result)
+    
+    return result
