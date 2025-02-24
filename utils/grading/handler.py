5cab7cd0eef14a577e19a2b83863038e7c03e8e3
prb-dev <prabashwara.in@gmail.com>
2025-02-24 08:31:15 +0530

    Add ER grading to the handler

--- utils/grading/handler.py
+++ utils/grading/handler.py
@@ -1,7 +1,12 @@
 from utils.grading.class_diagram_grading import get_class_marks
 from utils.grading.usecase_diagram_grading import get_usecase_marks
+from utils.grading.er_diagram_grading import get_er_marks
 
-functions = {"class": get_class_marks, "usecase": get_usecase_marks}
+functions = {
+    "class": get_class_marks,
+    "usecase": get_usecase_marks,
+    "er": get_er_marks,
+}
 
 
 def marks_invoker(type, correct, student, rubric):
