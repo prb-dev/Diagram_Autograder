a69eabebdffc2a0b3fa6b32441c8ec856170cbc5
prb-dev <prabashwara.in@gmail.com>
2025-03-02 22:31:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from routes.answer import answer_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = ["http://localhost:5173"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
