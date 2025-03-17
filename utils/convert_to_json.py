53c399dac3170cd654c82c9ef59d5c0fba7396ec
Prabashwara <prabashwara.in@gmail.com>
2025-03-17 20:08:28 +0530

    Update text generation to inference (#7)

--- utils/convert_to_json.py
+++ utils/convert_to_json.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_json(text: str):
-    text = text.strip().replace("json", "").replace("`", "").strip()
+    text = text.strip().replace("```json", "").replace("```", "").strip()
 
     try:
         json_obj = json.loads(text)
