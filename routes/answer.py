e54df1daee8fc33231a8d38fff38de7858d96b91
prb-dev <prabashwara.in@gmail.com>
2025-03-19 10:43:47 +0530

    Comment out jwt verifications

--- routes/answer.py
+++ routes/answer.py
@@ -13,7 +13,7 @@ answer_router = APIRouter()
 
 @answer_router.post("/answers/submit/{qid}/{uid}")
 def submit_answer(request: Request, qid: str, uid: str, url: str = Body(...)):
-    verify_token(request)
+    # verify_token(request)
     res = save_answer(qid, uid, url)
 
     return {"message": res["message"], "aid": res["aid"]}
@@ -21,21 +21,21 @@ def submit_answer(request: Request, qid: str, uid: str, url: str = Body(...)):
 
 @answer_router.get("/answers/{qid}")
 def retrieve_answers(request: Request, qid: str):
-    verify_token(request)
+    # verify_token(request)
     res = get_answers(qid)
     return {"answers": res["answers"]}
 
 
 @answer_router.get("/answers/{qid}/{aid}")
 def retrieve_answer(request: Request, qid: str, aid: str):
-    payload = verify_token(request)
-    verify_permission(payload)
+    # payload = verify_token(request)
+    # verify_permission(payload)
     res = get_answer(qid, aid)
     return {"answer": res["answer"]}
 
 
 @answer_router.get("/{sid}/answers")
 def retrieve_answers_by_student_id(request: Request, sid: str):
-    verify_token(request)
+    # verify_token(request)
     res = get_answer_by_student_id(sid)
     return {"answers": res["answers"]}
