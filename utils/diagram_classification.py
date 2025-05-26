8ce37d61af440331444c1968be7b59aeef2f21e9
prb-dev <prabashwara.in@gmail.com>
2025-05-27 05:08:10 +0530

    Update diagram_classification.py

--- utils/diagram_classification.py
+++ utils/diagram_classification.py
@@ -11,23 +11,38 @@ INFER_URL = os.getenv("DEBERTA_INFER_URL")
 def classify_diagram(question):
     headers = {"Authorization": f"Bearer {TOKEN}"}
 
-    for _ in range(3):
-        response = requests.post(
-            INFER_URL,
-            headers=headers,
-            json={
-                "inputs": question,
-                "parameters": {
-                    "candidate_labels": ["class", "usecase", "er", "sequence"]
-                },
-            },
-            timeout=10,
-        )
+    # for _ in range(3):
+    #     response = requests.post(
+    #         INFER_URL,
+    #         headers=headers,
+    #         json={
+    #             "inputs": question,
+    #             "parameters": {
+    #                 "candidate_labels": ["class", "usecase", "er", "sequence"]
+    #             },
+    #         },
+    #         timeout=10,
+    #     )
 
-        if response.status_code == 200:
-            return response.json()["labels"][0]
+    #     if response.status_code == 200:
+    #         return response.json()["labels"][0]
 
-    raise Exception("Failed to classify diagram")
+    # raise Exception("Failed to classify diagram")
+    response = requests.post(
+        INFER_URL,
+        headers=headers,
+        json={
+            "inputs": question,
+            "parameters": {"candidate_labels": ["class", "usecase", "er", "sequence"]},
+        },
+    )
+
+    if response.status_code == 200:
+        print(response.json())
+        return response.json()["labels"][0]
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        raise Exception("Failed to classify diagram")
 
 
 # def classify_diagram(question):
